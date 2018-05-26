ba4bc0c409011c51b752b59945257ad9ab042510
manuellouie <louieManuel238@gmail.com>
2018-05-25 22:05:29 -0600

    new Project; First commit

---
+++ BinaryTree.java
@@ -0,0 +1,15 @@
+//Helper Class Node
+class Node{
+	int value;
+	Node left;
+	Node right;
+
+	Node(int value){
+		this.value = value;
+		right = null;
+		left = null;
+	}
+}
+public class BinaryTree{
+	Node root;
+}
