9816238a2d5287605d5765fad62d29b4d63a2a8e
manuellouie <louieManuel238@gmail.com>
2018-05-26 23:42:38 -0600

    Completed methods (Traversals & delete)

--- BinaryTree.java
+++ BinaryTree.java
@@ -11,5 +11,185 @@ class Node{
 	}
 }
 public class BinaryTree{
+
 	Node root;
+
+
+	/**
+	 * This method will recurse through the tree until the current is null
+	 * 
+	 * @param current The current node in the recursion
+	 * @param value The value to be added
+	 * @return A newly created Node assigned to be the left / right of a leaf Node
+	 **/
+	private Node addRecursion(Node current, int value){
+		if(current == null)
+			return new Node(value);
+
+		if(value < current.value)
+			current.left = addRecursion(current.left, value);
+		else if(value > current.value)
+			current.right = addRecursion(current.right, value);
+		else
+			return current;
+		return current;
+	}
+
+	/**
+	 * Used to add a new element in the tree
+	 *
+	 * @param The value to be added
+	 **/
+	public void add(int value){
+		root = addRecursion(root, value);
+	}
+
+	/**
+	 * This method will recurse 
+	 *
+	 *
+	 * @param current The current node in the recursion
+	 * @param value the value to be deleted
+	 * @return Node
+	 **/
+	private Node deleteRecursion(Node current, int value){
+		if(current == null)
+			return null;
+
+		if(value == current.value){
+			// current is a leaf node
+			if(current.left == null && current.right == null)
+				return null;
+			// current has child on the left
+			if(current.right == null)
+				return current.left;
+			// current has child on the right
+			if(current.left == null)
+				return current.right;
+			// current has left and right child
+			if(current.left != null && current.right != null){
+				// replace the value to be deleted with the smallest value of its right sub tree
+				int smallestValue = findSmallestValue(current.right);
+				current.value = smallestValue;
+				// delete the smallest value of the right sub tree 
+				current.right = deleteRecursion(current.right, smallestValue);
+				return current;
+			}
+		}
+		if(value < current.value){
+			current.left = deleteRecursion(current.left, value);
+		}
+		current.right = deleteRecursion(current.right, value);
+		return current;
+	}
+
+	/**
+	 * Find the smallest value of a tree; 
+	 * traversing the left branch since the left will always have smaller values than the right
+	 *
+	 * @param the starting node of traversal
+	 * @return int smallest value
+	 **/
+	private int findSmallestValue(Node root){
+		return root.left == null ? root.value : findSmallestValue(root.left);
+	}
+
+	/**
+	 * Used to delete an element in the tree
+	 *
+	 * @param value The value to be deleted
+	 **/
+	public void delete(int value){
+		deleteRecursion(root, value);
+	}
+
+
+	/**
+	 * In-order Traversal
+	 * <p>
+	 * 		traverse left => visit node => traverse right
+	 * </p>
+	 * @param node
+	 **/
+	public void inOrderTraversal(Node node){
+		if(node == null)
+			return;
+		inOrderTraversal(node.left);
+		System.out.print(" "+ node.value);
+		inOrderTraversal(node.right);
+	}
+
+	/**
+	 * Pre-order Traversal
+	 * <p>
+	 * 		visit node => traverse left => traverse right
+	 * </p>
+	 * @param node
+	 **/
+	public void preOrderTraversal(Node node){
+		if(node == null)
+			return;
+		System.out.print(" "+ node.value);
+		preOrderTraversal(node.left);
+		preOrderTraversal(node.right);
+	}
+
+	/**
+	 * Post-order Traversal
+	 * <p>
+	 * 		traverse left => traverse right => visit node
+	 * </p>
+	 * @param node
+	 **/
+	public void postOrderTraversal(Node node){
+		if(node == null)
+			return;
+		postOrderTraversal(node.left);
+		postOrderTraversal(node.right);
+		System.out.print(" "+ node.value);
+	}
+
+	/**
+	 * Depth-first search (Itterative)
+	 *
+	 *
+	 *
+	 *
+	 **/
+	public int depthFirstSearch(int value){
+		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * Breadth-first search (Itterative)
+	 *
+	 *
+	 *
+	 *
+	 **/
+	public int breadthFirstSearch(int value){
+		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+	}
+
+	
+	public static void main(String[] args) {
+		BinaryTree bt = new BinaryTree();
+		bt.add(6);
+		bt.add(4);
+		bt.add(8);
+		bt.add(3);
+		bt.add(5);
+		bt.add(7);
+		bt.add(9);
+		bt.inOrderTraversal(bt.root);
+		System.out.println("");
+		bt.preOrderTraversal(bt.root);
+		System.out.println("");
+		bt.postOrderTraversal(bt.root);
+		System.out.println("");
+
+		bt.delete(6);
+		bt.inOrderTraversal(bt.root);
+		System.out.println("");
+	}
 }
